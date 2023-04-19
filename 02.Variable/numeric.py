7e073af7977a8ae5c362df74238273e5a260dce5
Paula Vieira <eipvi@proton.me>
2023-04-19 15:57:11 -0300

    variable numeric

---
+++ 02.Variable/numeric.py
@@ -0,0 +1,89 @@
+#Exemplo 1 de Variáveis numéricas:
+idade = 30
+preco = 9.99
+numero_complexo = 2 + 3j
+
+soma = idade + preco
+divisao = preco / idade
+produto = idade * numero_complexo
+
+print("Soma:", soma)
+print("Divisão:", divisao)
+print("Produto:", produto)
+
+
+#Exemplo 2 de Variáveis numéricas:
+# Declaração de uma variável numérica inteira
+x = 10
+
+# Imprime o valor da variável
+print("Valor de x:", x)
+
+# Realiza algumas operações matemáticas com a variável
+dobro = x * 2
+quadrado = x ** 2
+raiz_quadrada = x ** 0.5
+
+# Imprime os resultados das operações
+print("Dobro de x:", dobro)
+print("Quadrado de x:", quadrado)
+print("Raiz quadrada de x:", raiz_quadrada)
+
+
+#Exemplo 3 de Variáveis numéricas:
+idade = 30
+preco = 9.99
+numero_complexo = 2 + 3j
+
+soma = idade + preco
+divisao = preco / idade
+produto = idade * numero_complexo
+
+print("Soma:", soma)
+print("Divisão:", divisao)
+print("Produto:", produto)
+
+
+#Neste exemplo, a variável x foi declarada como um número inteiro (10). Em seguida, são realizadas algumas operações matemáticas com essa variável, como o cálculo do dobro, do quadrado e da raiz quadrada de x. Por fim, os resultados das operações são impressos na tela.
+
+#Lembre-se de que em Python, as variáveis numéricas podem ser de diferentes tipos, como inteiros (int), números reais com casas decimais (float) e números complexos (complex). O tipo da variável é definido automaticamente de acordo com o valor atribuído a ela.
+
+
+
+#Exemplo 4 de Variáveis numéricas:
+4 % 2 = 0 (0= número par)
+5 % 2 = 1 (1= número impar)
+
+#Exemplo 5 de Variáveis numéricas:
+5 / 2 =2.5
+5 // 2 =2 
+
+#Exemplo 6 de Variáveis numéricas:
+num = 42
+num (vai retornar o 42)
+
+print(num)
+Retornará o 42
+
+
+#Exemplo 7 de Variáveis numéricas:
+2 ** 8 =256 #dois elevado a 8 =256
+3 ** 3 =27
+
+
+#Exemplo 8 de Variáveis numéricas:
+num + 1 / print(num + 1)
+=43 #pegaria o num, somaria com mais 1, retornando 43
+
+
+#Exemplo 9 de Variáveis numéricas:
+num = 42
+type(num)
+<class 'int'> #retornou numero int
+
+
+#Exemplo 10 de Variáveis numéricas:
+num = 1_000_000
+
+print(num)
+=1000000 #numero sem barra
